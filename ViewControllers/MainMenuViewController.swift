c3a93b33d17145fa667f2565b93bba028776131b
Rebecca <rebeccaplease@gmail.com>
2015-07-13 15:03:08 -0400

    added day of the week buttons

---
+++ ViewControllers/MainMenuViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MainMenuViewController.swift
+//  AlarmPet
+//
+//  Created by Rebecca Poch on 7/13/15.
+//  Copyright (c) 2015 Rebecca Poch. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new View Controller using segue.destinationViewController.
+        // Pass the selected object to the new View Controller.
+        
+        if (segue.identifier == "Alarm") {
+        
+            let alarmViewController = segue.destinationViewController as! AlarmViewController
+            
+        }
+    }
+    @IBAction func unwindToSegue(segue: UIStoryboardSegue) {
+        println("unwinding")
+    }
+
+}
