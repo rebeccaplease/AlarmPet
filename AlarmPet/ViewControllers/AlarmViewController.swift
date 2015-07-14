94e5d3ec0ca456e399d8280fa4f4ae21f2b31549
Rebecca <rebeccaplease@gmail.com>
2015-07-14 15:33:53 -0400

    added custom notification buttons

--- AlarmPet/ViewControllers/AlarmViewController.swift
+++ AlarmPet/ViewControllers/AlarmViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class AlarmViewController: UIViewController {
 
-    //var saveAlarmTime: NSDate = NSDate()
+    var saveAlarmTime: NSDate = NSDate()
     
     //The date and time when the system should deliver the notification.
     //@NSCopying var fireDate: NSDate?
+    
     
     //MARK: Date functions
     @IBOutlet weak var sunday: UIButton!
@@ -32,10 +33,42 @@ class AlarmViewController: UIViewController {
     @IBOutlet weak var savedTime: UILabel!
     
     @IBAction func saveButtonPressed(sender: AnyObject) {
-        //saveAlarmTime = datePicker.date
         
-        savedTime.text = AlarmViewController.dateFormatter.stringFromDate(datePicker.date)
+    
+     //   var seconds = Double( NSDateComponents().second )
         
+       // saveAlarmTime = datePicker.date.dateByAddingTimeInterval(-seconds)
+        
+        saveAlarmTime = datePicker.date
+        
+        savedTime.text = AlarmViewController.dateFormatter.stringFromDate(saveAlarmTime)
+        
+        scheduleNotification()
+    }
+    
+    func scheduleNotification(){
+        println(saveAlarmTime)
+        
+        /*
+        
+        var notification = UILocalNotification()
+        notification.alertBody = "Todo Item \"\(item.title)\" Is Overdue" // text that will be displayed in the notification
+        notification.alertAction = "open" // text that is displayed after "slide to..." on the lock screen - defaults to "slide to view"
+        notification.fireDate = item.deadline // todo item due date (when notification will be fired)
+        notification.soundName = UILocalNotificationDefaultSoundName // play default sound
+        notification.userInfo = ["UUID": item.UUID, ] // assign a unique identifier to the notification so that we can retrieve it later
+        notification.category = "TODO_CATEGORY"
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+*/
+        var notification = UILocalNotification()
+        notification.fireDate = saveAlarmTime
+        notification.timeZone  = NSTimeZone.defaultTimeZone()
+        notification.alertBody = "Virtual pet in danger!"
+        notification.alertAction = "open"
+        notification.soundName = UILocalNotificationDefaultSoundName // play default sound
+        notification.category = "CATEGORY"
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
     
     static var dateFormatter: NSDateFormatter = {
