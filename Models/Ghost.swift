06f42cb132a5451459fabb150029dbdbc5415e5f
Rebecca <rebeccaplease@gmail.com>
2015-08-03 14:09:24 -0400

    separated ghost class

---
+++ Models/Ghost.swift
@@ -0,0 +1,21 @@
+//
+//  Ghost.swift
+//  AlarmPet
+//
+//  Created by Rebecca Poch on 8/3/15.
+//  Copyright (c) 2015 Rebecca Poch. All rights reserved.
+//
+
+import Foundation
+
+
+class Ghost {
+    
+    var id: Int
+    var dead: Bool
+    
+    init(id: Int) {
+        self.id = id
+        self.dead = false
+    }
+}
